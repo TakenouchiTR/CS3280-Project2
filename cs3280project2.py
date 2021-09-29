7c21db3aa50c4976cc90bb4db884ef06d556123f
TakenouchiTR <draconianlds@gmail.com>
2021-09-29 18:25:16 -0400

    Add and start HTTP server upon running

--- cs3280project2.py
+++ cs3280project2.py
@@ -8,10 +8,16 @@ __author__ = 'CS3280'
 __version__ = 'Fall 2021'
 __pylint__ = 'v1.8.3'
 
+class Project2Server(http.server.BaseHTTPRequestHandler):
+    pass
+
 def main():
-    result = utils.get_links("https://www.westga.edu")
-    for line in result:
-        print(line)
+    """
+    Entry point for the script
+    """
+    server_address_tuple = ('localhost', 3280)
+    server = http.server.HTTPServer(server_address_tuple, Project2Server)
+    server.serve_forever()
 
 if __name__ == "__main__":
     main()
