00dddd9ac688392f47ae454db8073a9f65767692
TakenouchiTR <draconianlds@gmail.com>
2021-10-01 23:11:00 -0400

    Add documentation to FakeResponse

--- utils_tests.py
+++ utils_tests.py
@@ -11,9 +11,22 @@ __version__ = "Fall 2021"
 __pylint__ = "v1.8.3"
 
 class FakeResponse:
+    #pylint: disable=too-few-public-methods
+    """
+    A fake response object used for providing a consistent input to
+    the utils module.
+    """
     text = ""
 
     def get(url):
+        #pylint: disable=unused-argument
+        #pylint: disable=no-self-argument
+        #pylint: disable=no-self-use
+        """
+        Creates a FakeResponse with multiple anchor tags as the text
+        Args: url - Unused variable to match the requests get() function
+        Return: A FakeResponse with text for testing
+        """
         response = FakeResponse()
         text = "<a href='https://www.example.com'></a>"
         text += "<a href='tel:1234567890'></a>"
