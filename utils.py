e104aaec537cea4bbc417fc72d7fd2390ccef93d
TakenouchiTR <draconianlds@gmail.com>
2021-10-01 21:24:20 -0400

    Add option to use custom request handler

--- utils.py
+++ utils.py
@@ -12,15 +12,16 @@ __author__ = "Shawn Carter"
 __version__ = "Fall 2021"
 __pylint__ = "v1.8.3"
 
-def get_links_from_url(url):
+def get_links_from_url(url, request_handler = requests):
     """
     Retrieves all of the links from the website at the specified URL
-    Args: url - The URL to search
+    Args: url - The URL for the webpage to search
+          request_handler - The module or class that will handle making requests.
     Return: The list of links from the website
     """
     href_regex = re.compile(r"(.*://)?(.*)")
 
-    response = requests.get(url)
+    response = request_handler.get(url)
     soup = bs4.BeautifulSoup(response.text, features="html.parser")
     tags = soup.find_all("a", href=True)
     links = []
